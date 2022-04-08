a0488f76fd3df07fe2b526f0d541e8eab26a02fd
Aleksandr Kravchuk <alexandrokrk@gmail.com>
2022-04-08 19:04:12 +0300

    2

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,6 +1,36 @@
 console.log(document)
-const numberList = document.querySelector(".item");
-
+const numberList = document.querySelectorAll(".item");
+// console.log(numberList.firstElementChild)
 // function numberOfCategories(numberList) {
 //     return AllquerySelector.numberList;
 // }
+// const listWithId = document.querySelector('#categories');
+// listWithId.style.textTransform = 'uppercase';
+// listWithId.style.fontSize = '24px';
+// console.log(listWithId);
+
+const categoriesNamber = document.querySelectorAll("h2");
+const categoriesName = document.querySelector("h2");
+const categoriesPosition = document.querySelectorAll("li");
+// firstMenuItem.style.color = 'tomato';
+const categories = categoriesNamber.length;
+const categoriesN = categoriesName.textContent;
+
+
+console.log(categoriesPosition)
+console.log(`Number of categories:${categories}`);
+// console.log(`Category:${categoriesN}
+// Elements:${}`);
+// console.log(`Category:${categoriesName}`)
+
+
+
+
+
+// const menuItemsByTagName = document.querySelectorAll("li");
+
+
+// console.log(menuItemsByTagName.getAttribute(li));
+
+
+// console.log(numberList)
