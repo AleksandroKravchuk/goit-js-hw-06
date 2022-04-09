d679608fed197761d7acdc9beefa1e1e83f99942
Aleksandr Kravchuk <alexandrokrk@gmail.com>
2022-04-09 16:31:14 +0300

    2

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -3,38 +3,44 @@ console.log(document)
 const categories = document.querySelector(`#categories `)
 const categoriesEl = document.querySelectorAll(".item");
 const categoriesTitle = document.querySelectorAll(".item h2");
-const categoriesItem = document.querySelectorAll(".item ul");
+const categoriesItem = document.querySelectorAll(".item  ul ");
 // const listEl = categoriesItem[].querySelectorAll(`li`);
 
-console.log(categoriesEl)
-console.log(categoriesItem)
+
+
+console.log(categories)
+console.log(categoriesItem.classList.add(`hdhdj`))
 
 const categoriesNumb = categoriesEl.length;
 console.log(`Number of categories:${categoriesNumb}`);
 
 
-const calkCategoriesTitle = (items,lists) => {
-    
+const calkCategoriesTitle = (items) => {
+ 
+       
+   
     for (let item of items) {
-        console.log(`Category:${item.textContent}`) 
-    }
-    for (let list of lists) {
-        console.log(`Elements:${list.textContent}`) 
+        console.log(`Category:${item.textContent}`)
+        // for (let list of lists) {
+        //     console.log(`Elements:${list}`) 
+        // }
     }
 }
-calkCategoriesTitle(categoriesTitle,categoriesItem);
+calkCategoriesTitle(categoriesTitle);
 
 
 
-// const calkCategoriesEl = (items)=> {
-//     for (const item of items) {
-//         console.log(`Elements:${item.length}`) 
-//     }
-// }
-// calkCategoriesEl( categoriesItem);
-// const considersItems = () => {
-    
-// }
+
+
+
+
+const calkCategoriesEl = (items)=> {
+    for (const item of items) {
+        console.log(`Elements:${item.firstElementChild}`) 
+    }
+}
+calkCategoriesEl( categoriesItem);
+
 
 
 // console.log(listEl)
