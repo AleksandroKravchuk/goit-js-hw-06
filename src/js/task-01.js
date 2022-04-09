f2c5b1ed79c03bf04fd8f183377616036ea95c31
Aleksandr Kravchuk <alexandrokrk@gmail.com>
2022-04-09 12:00:35 +0300

    2

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,12 +1,46 @@
 console.log(document)
 
+const categories = document.querySelector(`#categories `)
+const categoriesEl = document.querySelectorAll(".item");
+const categoriesTitle = document.querySelectorAll(".item h2");
+const categoriesItem = document.querySelectorAll(".item ul");
+// const listEl = categoriesItem[].querySelectorAll(`li`);
 
-const categoriesNamber = document.querySelectorAll("h2");
+console.log(categoriesEl)
+console.log(categoriesItem)
 
-const categoriesPosition = document.querySelectorAll("li");
-
-const categories = categoriesNamber.length;
-
+const categoriesNumb = categoriesEl.length;
+console.log(`Number of categories:${categoriesNumb}`);
 
 
-console.log(`Number of categories:${categories}`);
+const calkCategoriesTitle = (items,lists) => {
+    
+    for (let item of items) {
+        console.log(`Category:${item.textContent}`) 
+    }
+    for (let list of lists) {
+        console.log(`Elements:${list.textContent}`) 
+    }
+}
+calkCategoriesTitle(categoriesTitle,categoriesItem);
+
+
+
+// const calkCategoriesEl = (items)=> {
+//     for (const item of items) {
+//         console.log(`Elements:${item.length}`) 
+//     }
+// }
+// calkCategoriesEl( categoriesItem);
+// const considersItems = () => {
+    
+// }
+
+
+// console.log(listEl)
+    
+
+
+// console.log(`Category:${calkCategoriesTitle(categoriesTitle)}`);
+// console.log(`Elements:${categoriesEl1}`)
+
