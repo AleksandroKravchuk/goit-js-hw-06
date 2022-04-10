18e960d37413694ed3215bd75876e8742b71d306
Aleksandr Kravchuk <alexandrokrk@gmail.com>
2022-04-10 18:20:39 +0300

    дз 2

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,15 +1,7 @@
-console.log(document)
 
-const categories = document.querySelector(`#categories `)
+
+
 const categoriesEl = document.querySelectorAll(".item");
-const categoriesTitle = document.querySelectorAll(".item h2");
-const categoriesItem = document.querySelectorAll(".item  ul ");
-// const listEl = categoriesItem[].querySelectorAll(`li`);
-
-
-
-console.log(categories)
-console.log(categoriesItem.classList.add(`hdhdj`))
 
 const categoriesNumb = categoriesEl.length;
 console.log(`Number of categories:${categoriesNumb}`);
@@ -17,36 +9,11 @@ console.log(`Number of categories:${categoriesNumb}`);
 
 const calkCategoriesTitle = (items) => {
  
-       
-   
     for (let item of items) {
-        console.log(`Category:${item.textContent}`)
-        // for (let list of lists) {
-        //     console.log(`Elements:${list}`) 
-        // }
-    }
+        console.log(`Category:${item.firstElementChild.textContent}
+       Elements: ${item.lastElementChild.children.length} `)    
 }
-calkCategoriesTitle(categoriesTitle);
-
-
-
-
-
-
-
-const calkCategoriesEl = (items)=> {
-    for (const item of items) {
-        console.log(`Elements:${item.firstElementChild}`) 
-    }
 }
-calkCategoriesEl( categoriesItem);
+calkCategoriesTitle(categoriesEl);
 
-
-
-// console.log(listEl)
-    
-
-
-// console.log(`Category:${calkCategoriesTitle(categoriesTitle)}`);
-// console.log(`Elements:${categoriesEl1}`)
 
--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const vegetableList = document.querySelector(`#ingredients`);
+
+function addVegetables(items,inx) {
+  for (let item of items) {
+
+item= document.createElement(`li`);
+item.classList.add(`item`);
+    item.textContent = ingredients[inx];
+    return item;
+    
+}
+  }
+vegetableList.append( addVegetables(ingredients,0),addVegetables(ingredients,1),addVegetables(ingredients,2),addVegetables(ingredients,3),addVegetables(ingredients,4),addVegetables(ingredients,5));
+
+console.log(vegetableList)
