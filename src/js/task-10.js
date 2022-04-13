3a21e074595a076ea066d1d51e789754149b0d31
Aleksandr Kravchuk <alexandrokrk@gmail.com>
2022-04-13 22:36:46 +0300

    dz 10

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,22 +1,14 @@
 const refs = {
-
-  form: document.querySelector(`#controls`),
   inputValue: document.querySelector(`[type="number"]`),
   btnCreate: document.querySelector(`[data-create]`),
   btnDestroy: document.querySelector(`[data-destroy]`),
   boxes: document.querySelector(`#boxes`),
 }
 
-// console.log(refs.form);
-// console.log(refs.inputValue);
-// console.log(refs.btnCreate);
-// console.log(refs.btnDestroy);
-// console.log(refs.boxes);
 
 refs.inputValue.addEventListener(`input`, amountElement);
 
 
-// let amount = 4;
 function amountElement(event) {
   if (event.currentTarget.value < 1 || event.currentTarget.value > 100) {
     alert`Введите число от 1 до 100`
@@ -24,25 +16,50 @@ function amountElement(event) {
   }
   const amountOfElements = event.currentTarget.value;
 
- console.log(refs.btnCreate.setAttribute(`index`,Number(amountOfElements) )) ;
-  
-  // console.log(refs.btnCreate.attributes)
- 
-  // console.log(refs.btnCreate.attributes.index);
-}
- refs.btnCreate.addEventListener(`click`, createBoxes);
-// const elements = refs.inputValue.addEventListener(`input`, amountElement);
-// console.log(elements);
-
-function createBoxes(amount) {
-  console.log(amount.currentTarget);
-  let element = ``;
-  for (let i = 1; i <= amount;i+=1)  {
-   element += `<div></div>`;
-  } boxes.append(element);
+  refs.btnCreate.setAttribute(`index`, amountOfElements);
 }
 
+
+refs.btnCreate.addEventListener(`click`, createBoxes);
+
+ const changeSizes = {
+    value: 20,
+    addValue() {
+      this.value += 10;
+    }
+  }
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+function createBoxes(amount) {
+  amount = refs.btnCreate.getAttribute(`index`);
+  let element = ``;
+  for (let i = 1; i <= amount; i += 1) {
+   changeSizes.addValue();
+   element = document.createElement(`div`);
+    element.style.width = `${changeSizes.value}px`; 
+    element.style.height = `${changeSizes.value}px`; 
+    element.style.backgroundColor = getRandomHexColor();
+
+  console.log(element);
+    refs.boxes.append(element);
+    
+  } 
+}
+
+  
+refs.btnDestroy.addEventListener(`click`, destroyBoxes);
+
+
+function destroyBoxes() {
+  refs.boxes.innerHTML = ``;
+}
+  
+  
+ 
+  
+  
+
